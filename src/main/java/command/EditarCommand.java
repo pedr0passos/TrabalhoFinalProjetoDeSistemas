f5234578c6149c9b399161f920a00d9bd8e545c7
Pedro Henrique Passos Rocha <pedro.rocha@optsol.com.br>
2024-09-14 19:29:18 -0300

    .

--- src/main/java/command/EditarCommand.java
+++ src/main/java/command/EditarCommand.java
@@ -1,14 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package command;
 
 import state.EdicaoState;
 
 /**
- *
- * @author pedro
+ * @author Catterina Vittorazzi Salvador
+ * @author Pedro Henrique Passos Rocha 
+ * @author João Victor Mascarenhas 
  */
 
 public class EditarCommand implements Command {
